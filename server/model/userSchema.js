6fedaed9c9fc75720051e1c0da9ce77c631f62af
Vishal Maurya <vishalmaurya3112@gmail.com>
2021-09-17 11:08:30 +0530

    added react

--- server/model/userSchema.js
+++ server/model/userSchema.js
@@ -1,5 +1,7 @@
 const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const e = require('express');
 const userSchema = new mongoose.Schema({
     username: {
         type: String,
@@ -28,18 +30,42 @@ const userSchema = new mongoose.Schema({
     phone: {
         type: Number,
         required: true,
-    }
+    },
+    tokens: [
+        {
+            token:{
+                type: String,
+                required: true,
+            }
+        }
+    ]
 })
 
 
 // Hashing of the password and confirm password
 userSchema.pre('save', async function(next) {
-    if(this.isModified()){
+    if(this.isModified('password')){
         this.password = await bcrypt.hash(this.password, 12);
         this.confirmpassword = await bcrypt.hash(this.confirmpassword, 12);
+        
     }
+    next();
+    
+    
+    
 })
 
+userSchema.methods.generateToken = async function () {
+    try{
+        let token = jwt.sign({_id: this._id}, process.env.SECRET_TOKEN);
+        this.tokens = this.tokens.concat({token: token});
+        await this.save();
+        return token;
+    }
+    catch(err){
+        console.log(err);
+    }
+}
 
 const userModel = mongoose.model('user', userSchema);
 
--- server/router/auth.js
+++ server/router/auth.js
@@ -1,8 +1,8 @@
-const { Router } = require("express");
 const express = require("express");
 const userModel = require("../model/userSchema");
 const router = express.Router();
-require('../database/connection')
+require("../database/connection");
+const bcrypt = require("bcryptjs");
 
 router.get("/", (request, response) => {
   response.send("Hello World");
@@ -30,8 +30,8 @@ router.post("/register", (request, response) => {
   ) {
     return response.status(400).send({ error: "Please fill in all fields" });
   }
-  if(password !== confirmpassword){
-      return response.status(400).send({ error: "Passwords do not match" });
+  if (password !== confirmpassword) {
+    return response.status(400).send({ error: "Passwords do not match" });
   }
 
   userModel
@@ -48,10 +48,12 @@ router.post("/register", (request, response) => {
         confirmpassword,
         firstName,
         lastName,
-        phone
+        phone,
       });
 
-      user.save().then(() => {
+      user
+        .save()
+        .then(() => {
           return response.status(200).send({ message: "User Created" });
         })
         .catch((error) => {
@@ -63,28 +65,36 @@ router.post("/register", (request, response) => {
     });
 });
 
-
 router.post("/login", async (request, response) => {
-    try{
-        const { username, password} = request.body;
-        if(!username || !password){
-            return response.status(400).json({ error: "Please fill in all fields" });
-        }
-        console.log(username, password);
-        const validUser = await userModel.findOne({ username });
-        if(!validUser){
-            return response.status(400).json({ error: "User Not Found" });
-        }
-
-        if(validUser){
-            if(validUser.password !== password){
-                return response.status(400).json({ error: "Password is incorrect" });
-            }
-            return response.status(200).json({ message: "User logged in" });
-        }
-    }   
-    catch(error){
-        return response.status(500).json({ error: error });
+  try {
+    let token;
+    const { username, password } = request.body;
+    if (!username || !password) {
+      return response.status(400).json({ error: "Please fill in all fields" });
     }
-})
+
+    const validUser = await userModel.findOne({ username });
+    if (!validUser) {
+      return response.status(400).json({ error: "User Not Found" });
+    }
+    
+    const passwordMatch = await bcrypt.compare(password, validUser.password);
+
+    
+    if (validUser) {
+      if (passwordMatch) {
+        token = await validUser.generateToken();
+        response.cookie("login_token", token, {
+              expires: new Date(Date.now() + 1296000000),
+              httpOnly: true,
+            });
+        return response.status(200).json({ message: "User logged in" });
+      } else {
+        return response.status(400).json({ error: "Password is incorrect " + passwordMatch });
+      }
+    }
+  } catch (error) {
+    return response.status(500).json({ Error: error });
+  }
+});
 module.exports = router;
